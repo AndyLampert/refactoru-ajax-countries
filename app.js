d8089f0566af1bfdc9acd307b7ec951749a61ccd
AndyLampert <lamperta@gmail.com>
2014-05-13 12:25:43 -0600

    stored inputs in an object

--- app.js
+++ app.js
@@ -16,6 +16,35 @@ app.get('/countries', function(req, res){
 	res.send(countryData);
 })
 
+app.post('/search', function(req, res){
+
+	// console.log(
+	// 	{
+	// 		countryName: req.body.countryName
+	// 	});
+	
+	var searchVal = req.body.countryName
+
+	// Filter the countries array to only the names that match the user's search query, then send the matching countries back to the client.
+
+	// testFunc() --> function to test each element in an array
+	// country is the loop variable (one of countryData)
+	function testFunc(country) {
+		// argument to filter func is countryData[i]
+		// if a part of searchVal exists in country.name
+		if(country.name.indexOf(searchVal) > -1){
+			return true;
+		}
+	}
+
+	var filterResult = countryData.filter(testFunc);
+	// console.log(filterResult);
+	
+	// sending back filtered result. add [0] for just one result
+	res.send(filterResult);
+	// req.body -- will access object['countryName'] (or object.countryName)
+});
+
 var server = app.listen(6183, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	$(document).on('click', '.load-countries-btn', function(){
 		var itemEl = $(this);
@@ -10,9 +9,31 @@ $(document).ready(function(){
 			});
 			list.append(listItems);
 			$('body').append(list);
-
 		})
+	});
+	$(document).on('click', '.search-btn', function(){
+		var requestData = {
+			countryName: $('.country-input').val()
+		};
+		$.post('/search', requestData, 
+		// 3rd param is the callback with the response data
+		function(responseData){
+			// loop through responseData (already filtered) results
+			var resultCountryNames = [];
+				for (var i = 0; i < responseData.length; i++) {
+				resultCountryNames.push(responseData[i].name);
+			};
+			console.log(resultCountryNames);
+			var resultList = $('<ul>');
+			var resultListItems = responseData.map(function(country){
+				return $('<li class=country-result-name>' + country.name + '</li>');
+			});
+			resultList.append(resultListItems);
+			$('body').append(resultList);
+		});
 	});
 });
 
 // In your Ajax callback, loop through each country in the response and render a list item for each one into an unordered list on the page.
+
+// 
